3badac538a16cbe45ffb3106e07f19342e018144
janani <janani010403@gmail.com>
2024-05-27 14:41:38 +0530

    first commit

---
+++ simple_quickstart/src/test/java/janani/simple_quickstart/AppTest.java
@@ -0,0 +1,20 @@
+package janani.simple_quickstart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
